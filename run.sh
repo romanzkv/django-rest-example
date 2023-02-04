512b0696e12621d276afc92de19758b1d7f853e7
root <roman.z@qspark.co>
2023-02-04 23:36:10 +0000

    added a run variant to open interactive bash session into the container

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 docker build -t django_tutorial . && \
-docker run -v $(pwd)/src:/app --rm --init django_tutorial
+
+# use this run just to run the container
+#docker run -v $(pwd)/src:/app --rm --init django_tutorial
+
+# use this run to run the container and open an interactive bash session into it
+docker run -it -v $(pwd)/src:/app --rm --init django_tutorial bash
