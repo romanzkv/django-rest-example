9ff10ca46c2c3834caf74d5af672201c2b3fd0d0
root <roman.z@qspark.co>
2023-02-05 22:32:40 +0000

    added a check if container not found

--- stop.sh
+++ stop.sh
@@ -2,6 +2,10 @@
 
 # this script stops the container
 
-set -x
+# set -x
 container_id=$(docker ps | rg django_tutorial | awk '{print $1}')
+if [ -z "$container_id" ]; then
+    echo "countainer not found"
+    exit 1
+fi
 docker stop -t 0 $container_id
