b8dd303077a9dd4a02eaf08bd981503d156d93cb
root <roman.z@qspark.co>
2023-02-06 15:18:21 +0000

    added a serializer to to retrn real data on GET

--- src/tut1/api/views.py
+++ src/tut1/api/views.py
@@ -1,11 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from app1.models import Person
+from .serializers import PersonSerializer
 
 @api_view(['GET'])
-def getPesons(request):
+def getPeople(request):
     person = dict(name="roman", age=52)
-    return Response(person)
+    people = Person.objects.all()
+    serializer = PersonSerializer(people, many=True)
+    return Response(serializer.data)
 
 
 
