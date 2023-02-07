9e29863eac7710304344dc99a390293916a11c23
root <roman.z@qspark.co>
2023-02-07 16:44:59 +0000

    switch from function based to class based views
    i had to use class based views because our code uses class based
    views and i want to test the bulks on the same structure

--- src/tut1/api/urls.py
+++ src/tut1/api/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'people', views.PeopleViewSet)
+
 urlpatterns = [
-    path('', views.getPeople),
-    path('create/', views.addPeople),
+    # path('', views.getPeople),
+    # path('create/', views.addPeople),
+    path('', include(router.urls)),
 ]
--- src/tut1/api/views.py
+++ src/tut1/api/views.py
@@ -1,21 +1,42 @@
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from app1.models import Person
 from .serializers import PersonSerializer
 
-@api_view(['GET'])
-def getPeople(request):
-    person = dict(name="roman", age=52)
-    people = Person.objects.all()
-    serializer = PersonSerializer(people, many=True)
-    return Response(serializer.data)
+# these are function based views, they are working well
+# bug out project uses class based views so i have to
+# implement bulk insert using class based views
+#
+#@api_view(['GET'])
+#def getPeople(request):
+#    person = dict(name="roman", age=52)
+#    people = Person.objects.all()
+#    serializer = PersonSerializer(people, many=True)
+#    return Response(serializer.data)
+#
+#@api_view(['POST'])
+#def addPeople(request):
+#    serializer = PersonSerializer(data = request.data, many=True)
+#    if serializer.is_valid():
+#        serializer.save()
+#    return Response(serializer.data)
 
-@api_view(['POST'])
-def addPeople(request):
-    serializer = PersonSerializer(data = request.data, many=True)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+class PeopleViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
 
-
-
+    # all REST operations are implemented in the base class, 
+    # however they only handle single objects. To handle arrays, 
+    # I have overridden the 'create' method (which handles the POST verb) 
+    # to check if the data is an array. If it is, I handle it myself. 
+    # If the data is not an array, I pass it to the base class for processing
+    def create(self, request, *args, **kwargs):
+        if isinstance(request.data, list):
+            serializer = self.get_serializer(data=request.data, many=True)
+            if serializer.is_valid():
+                self.perform_create(serializer)
+                return Response(serializer.data)
+            return Response(serializer.errors)
+        else:
+            return super().create(request, *args, **kwargs)
